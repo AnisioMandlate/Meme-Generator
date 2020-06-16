8b9c4b5d3fe9fda3489aa10de8726fc1aecaa749
Anisio Mandlate <anisiomandlate@gmail.com>
2020-06-16 17:08:19 +0200

    feat: Updated the events to use arrow functions

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -4,32 +4,29 @@ import Form from "./Form";
 class MemeGenerator extends Component {
   constructor() {
     super();
-
     this.state = {
       topText: "",
       bottomText: "",
       randomImg: "http://i.imgflip.com/1bij.jpg",
       allMemeImgs: [],
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
-  }
+  };
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
     const randomNumber = Math.floor(
       Math.random() * this.state.allMemeImgs.length
     );
     const randomMemeImg = this.state.allMemeImgs[randomNumber].url;
     this.setState({ randomImg: randomMemeImg });
-  }
+  };
 
   componentDidMount() {
     fetch("https://api.imgflip.com/get_memes")
